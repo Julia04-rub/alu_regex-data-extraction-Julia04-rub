b36d9e84c1c7fd62a39fa58baaa7e2c4644fee91
Julia04-rub <j.rubibi@alustudent.com>
2025-05-19 05:59:26 -0700

    updated

---
+++ final-output.py
@@ -0,0 +1,6 @@
+Email Addresses: ['user@example.com', 'firstname.lastname@company.co.uk']
+Phone Numbers: ['(123) 456-7890', '123-456-7890', '123.456.7890']
+Credit Card Numbers: ['1234 5678 9012 3456', '1234-5678-9012-3456']
+HTML Tags: ['<p>', '<div class="example">', '<img src="image.jpg" alt="description">']
+Hashtags: ['#example', '#ThisIsAHashtag']
+
